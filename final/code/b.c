ca804d7e89efe130de0a2b465513bc379f91e09c
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-05-16 18:10:58 +0000

    final/b: implement

---
+++ final/code/b.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+enum { MAX_SIZE = 5300 };
+
+double A[MAX_SIZE][MAX_SIZE];
+double A2[MAX_SIZE][MAX_SIZE];
+
+int main() {
+    size_t r, c;
+    scanf("%ld %ld", &r, &c);
+    // read A
+    for (size_t i = 0; i < r; ++i) {
+        while (1) {
+            int j1;
+            scanf("%d", &j1);
+            if (j1 == -1) {
+                break;
+            }
+            size_t j = (size_t)(j1-1);
+            scanf("%lf", &A[i][j]);
+        }
+    }
+
+    // twice
+    for (int i = 0; i < r; ++i) {
+        for (int j = 0; j < c; ++j) {
+            for (int k = 0; k < r; ++k) {
+                A2[i][j] += A[i][k] * A[k][j];
+            }
+        }
+    }
+
+    // print
+    printf("%ld %ld\n", r, c);
+    for (size_t i = 0; i < r; ++i) {
+        for (size_t j = 0; j < c; ++j) {
+            if (A2[i][j] != 0) {
+                printf("%ld %lf ", j + 1, A2[i][j]);
+            }
+        }
+        printf("-1\n");
+    }
+
+    return 0;
+}
+
+/* 
+Samples
+=======
+In
+--
+3 2
+1 2.0 -1
+2 4.0 -1
+2 1.0 -1
+2 4
+1 3.0 4 1.0 -1
+2 4.0 3 2.0 4 3.0 -1
+
+Out
+---
+3 4
+1 6.0 4 2.0 -1
+2 16.0 3 8.0 4 12.0 -1
+2 4.0 3 2.0 4 3.0 -1
+
+*/
