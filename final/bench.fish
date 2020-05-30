84c3b99bda2663fcc1e5884dde2944b33ad0f593
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-05-30 02:49:09 +0000

    final: more measuring!

--- final/bench.fish
+++ final/bench.fish
@@ -7,7 +7,11 @@ function run_code
     pushd code
     if command test $program.c -nt $program.out
         echo - Compiling $program.c ...
-        gcc $program.c -Wno-unused-result -O3 -o $program.out
+        gcc $program.c -Werror -Wall -Wextra -Wconversion -Wno-unused-result -O3 -o $program.out
+        gcc $program.c -Werror -Wall -Wextra -Wconversion -Wno-unused-result -O3 -D TIMER -o $program.timer.out
+        if test $status -ne 0
+            exit
+        end
     end
     popd
     echo - Running $program ...
@@ -15,10 +19,31 @@ function run_code
         mkdir out/$program
     end
     true > bench/$program.txt
+
+    echo Measuring total time ...
     for i in (seq 1 10)
         set filename (printf "bcspwr%02d.txt" $i)
-        time code/$program.out <in/$filename >out/$program/$filename 2>> bench/$program.txt
+        time -a -o bench/$program.txt code/$program.out <in/$filename >out/$program/$filename
         diff out/$program/$filename out/ans/$filename
+        if test $status -ne 0
+            echo "answer is not correct";
+            exit
+        end
+    end
+    true > bench/$program.detail.txt;
+    echo Measuring detailed time ...
+    for i in (seq 1 10)
+        set filename (printf "bcspwr%02d.txt" $i)
+        eval ./code/$program.timer.out <in/$filename >/dev/null 2>>bench/$program.detail.txt
+    end
+
+    echo Measuring Memory usage ...
+    if test ! -e bench/$program.memory
+        mkdir bench/$program.memory
+    end
+    for i in (seq 1 10)
+        set filename (printf "bcspwr%02d.txt" $i)
+        valgrind 2>>/dev/null --tool=massif  --massif-out-file=bench/$program.memory/$i.txt code/$program.out  < in/$filename >/dev/null
     end
 end
 
