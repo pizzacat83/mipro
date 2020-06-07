374c9913381c3b2e1359b30452aa61858910aaf4
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2020-06-07 15:27:14 +0900

    include_guard: implement autofix

--- tests/include_guard.py
+++ tests/include_guard.py
@@ -2,40 +2,66 @@ from os import path
 import sys
 import glob
 import re
+import fileinput
 
-cwd = path.dirname(path.abspath(__file__))
+scriptdir = path.dirname(path.abspath(__file__))
+srcdir = path.join(scriptdir, '..') # このスクリプトのあるディレクトリから見たソースディレクトリの相対パス
 
-def check_file(abspath):
+rifndef = re.compile(r'^#ifndef\s+')
+rdefine = re.compile(r'^#define\s+')
+
+def check_file(abspath, autofix=False):
     errors = []
     relpath = f'./{path.relpath(abspath)}'
-    with open(abspath, 'r') as f:
-        l1 = f.readline().strip()
-        l2 = f.readline().strip()
-        filename = path.splitext(path.basename(abspath))[0]
-        rifndef = re.compile(r'^#ifndef\s+')
-        rdefine = re.compile(r'^#define\s+')
-        if rifndef.match(l1):
-            actual = re.sub(rifndef, '', l1, 1)
-            expected = f'H_{filename.upper()}'
-            if actual != expected:
-                errors.append(f'{relpath}:1: #ifndef symbol `{actual}` should be `{expected}`')
-        else:
-            errors.append(f'{relpath}:1: Missing #ifndef')
-        if rdefine.match(l2):
-            actual = re.sub(rdefine, '', l2, 1)
-            expected = f'H_{filename.upper()}'
-            if actual != expected:
-                errors.append(f'{relpath}:2: #define symbol `{actual}` should be `{expected}`')
-        else:
-            errors.append(f'{relpath}:2: Missing #define')
-        # TODO: check tail endif
-        # TODO: autofix
+    filename = path.splitext(path.basename(abspath))[0]
+    with fileinput.FileInput(abspath, inplace=autofix, backup='.bak') as f:
+        for i, line in enumerate(map(lambda s: s.strip('\n'), f)):
+            errprefix = f'{relpath}:{i + 1}:'
+            if i == 0:
+                expected = f'H_{filename.upper()}'
+                ok = True
+                if rifndef.match(line):
+                    actual = re.sub(rifndef, '', line, 1)
+                    if actual != expected:
+                        errors.append(f'{errprefix} #ifndef symbol `{actual}` should be `{expected}`')
+                        ok = False
+                else:
+                    errors.append(f'{errprefix} Missing #ifndef')
+                    ok = False
+                if autofix:
+                    if ok:
+                        print(line)
+                    else:
+                        print(f'#ifndef {expected}')
+            elif i == 1:
+                ok = True
+                expected = f'H_{filename.upper()}'
+                if rdefine.match(line):
+                    actual = re.sub(rdefine, '', line, 1)
+                    if actual != expected:
+                        errors.append(f'{errprefix} #define symbol `{actual}` should be `{expected}`')
+                        ok = False
+                else:
+                    errors.append(f'{errprefix} Missing #define')
+                    ok = False
+                if autofix:
+                    if ok:
+                        print(line)
+                    else:
+                        print(f'#define {expected}')
+            else:
+                if autofix:
+                    print(line)
+    # TODO: check tail endif
     return errors
 
 errors = []
+i = 0
+for filepath in map(path.normpath, glob.iglob(f'{srcdir}/*.h')):
+    errors += check_file(filepath, autofix=True)
+    i += 1
 
-for filepath in map(path.normpath, glob.iglob(f'{cwd}/../*.h')):
-    errors += check_file(filepath)
+print(f'Checked {i} file(s).', file=sys.stderr)
 
 if errors:
-    raise Exception('\n'.join(['Header check failed', *errors]))
+    raise Exception('\n'.join(['Header check failed', *errors])) # print all errors
